4b5be4fd28fcbaddbb45b161d8d0a181f4a4e1f7
xuhuang <xuhuang@theapollobox.com.cn>
2019-08-13 17:33:33 +0800

    add local rsk3-utils to abi

--- packages/rsk3-abi/src/index.js
+++ packages/rsk3-abi/src/index.js
@@ -1,6 +1,4 @@
-
-
-import * as Utils from 'web3-utils'; //TODO change to rsk3-utils
+import * as Utils from 'rsk3-utils'; // TODO change to rsk3-utils
 import {AbiCoder as EthersAbiCoder} from 'ethers/utils/abi-coder';
 import EthAbiCoder from './AbiCoder.js';
 
