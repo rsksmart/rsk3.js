0eb1f4c54024c51810489f8a0a141a761da949ce
chrisli30 <chrisli30@users.noreply.github.com>
2019-10-01 16:21:04 -0700

    Add web3-core-helpers to globals in rollup

--- packages/rsk3/rollup.config.js
+++ packages/rsk3/rollup.config.js
@@ -1,4 +1,6 @@
 import pkg from './package.json';
 import rollupConfig from '../../rollup.config';
 
-export default rollupConfig('Rsk3', pkg.name);
+export default rollupConfig('Rsk3', pkg.name,{
+    'web3-core-helpers':'web3-core-helpers',
+});
