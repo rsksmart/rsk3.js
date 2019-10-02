3b663430cf5f794731c5de7f6438fb3bc11ccf78
chrisli30 <chrisli30@users.noreply.github.com>
2019-10-02 16:25:55 -0700

    Updated demo index.js

---
+++ demo/index.js
@@ -0,0 +1,14 @@
+import Rsk3 from '../packages/rsk3/';
+// import {Rsk3} from 'rsk3';
+
+console.log(Rsk3);
+const result = rsk3.utils.isValidChecksumAddress('0xCd2a3D9f938e13Cd947EC05aBc7fE734df8dD826',33);
+
+console.log('result', result);
+
+// import web3 from 'web3';
+// console.log(web3);
+// const bn = new web3.utils.BN('0x15');
+// const number = bn.toNumber();
+
+// console.log('result', number);
