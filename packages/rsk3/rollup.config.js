0947a48d9393b09922861691d770da5ecb6dfb24
chrisli30 <chrisli30@users.noreply.github.com>
2019-10-01 19:11:12 -0700

    Updated rollup for rsk3 package

--- packages/rsk3/rollup.config.js
+++ packages/rsk3/rollup.config.js
@@ -3,4 +3,10 @@ import rollupConfig from '../../rollup.config';
 
 export default rollupConfig('Rsk3', pkg.name,{
     'web3-core-helpers':'web3-core-helpers',
+    'rsk3-account':'rsk3-account',
+    'rsk3-contract':'rsk3-contract',
+    'rsk3-personal':'rsk3-personal',
+    'rsk3-abi':'rsk3-abi',
+    'rsk3-net':'rsk3-net',
+    'rsk-utils':'rsk-utils'
 });
